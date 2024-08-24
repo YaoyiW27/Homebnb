21360b66f29839d536eab567de7354413bb4cd7c
Yaoyi Wang <129607949+YaoyiW27@users.noreply.github.com>
2024-08-24 11:05:46 -0700

    Update Stats.tsx

--- components/reservations/Stats.tsx
+++ components/reservations/Stats.tsx
@@ -8,7 +8,7 @@ async function Stats() {
     <div className='mt-8 grid md:grid-cols-2 gap-4 lg:grid-cols-3'>
       <StatsCards title='properties' value={stats.properties} />
       <StatsCards title='nights' value={stats.nights} />
-      <StatsCards title='total' value={formatCurrency(stats.amount) as number} />
+      <StatsCards title='total' value={stats.amount} />
     </div>
   );
 }
