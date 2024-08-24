e7862c9bf85869f92d04a04b9f3bc5150e5b7ce9
Yaoyi <wangyaoyi@Yaoyis-MacBook-Pro.local>
2024-08-23 19:07:40 -0700

    fix profile create page bug

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,15 +1,10 @@
+import { SubmitButton } from '@/components/form/Buttons';
 import FormContainer from '@/components/form/FormContainer';
 import FormInput from '@/components/form/FormInput';
-import { SubmitButton } from '@/components/form/Buttons';
-import {
-  updateProfileAction,
-  fetchProfile,
-  updateProfileImageAction,
-} from '@/utils/actions';
-import ImageInputContainer from '@/components/form/ImageInputContainer';
+import { createProfileAction } from '@/utils/actions';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
-async function ProfilePage() {
+async function CreateProfilePage() {
   const user = await currentUser();
 
   if (user?.privateMetadata?.hasProfile) redirect('/');
@@ -17,13 +12,7 @@ async function ProfilePage() {
     <section>
       <h1 className='text-2xl font-semibold mb-8 capitalize'> new user</h1>
       <div className='border p-8 rounded-md '>
-        <ImageInputContainer 
-          image={profile.prifileImage} 
-          name ={profile.username} 
-          action={updateProfileImageAction} 
-          text='Update Profile Image' 
-        />
-        <FormContainer action={updateProfileAction}>
+        <FormContainer action={createProfileAction}>
           <div className='grid md:grid-cols-2 gap-4 mt-4'>
             <FormInput type='text' name='firstName' label='First Name' />
             <FormInput type='text' name='lastName' label='Last Name' />
@@ -35,4 +24,4 @@ async function ProfilePage() {
     </section>
   );
 }
-export default ProfilePage;
+export default CreateProfilePage;
